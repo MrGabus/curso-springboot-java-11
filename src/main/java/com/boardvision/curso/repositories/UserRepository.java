6f0c3196e3084c26b5c6b10e76cc40f5697cddcd
Gabriel <gabrielsiodoni@hotmail.com>
2021-08-17 16:06:03 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/boardvision/curso/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.boardvision.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.boardvision.curso.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+}
